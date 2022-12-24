36f7d8f2e5ac3691fc4dbf0bca78bab8b44aa018
AchyutTiwari <achyut.tiwari_cs8@gla.ac.in>
2022-12-24 16:02:41 +0530

    model package

---
+++ spring-security-client/src/main/java/com/springSecurity/client/model/UserModel.java
@@ -0,0 +1,17 @@
+package com.springSecurity.client.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class UserModel {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String matchingPassword;
+}
