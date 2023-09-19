f6308a292e71e7cf629c452a8b6e11b474a8f253
AchyutTiwari <achyut.tiwari_cs8@gla.ac.in>
2023-09-19 18:56:32 +0530

    ...

--- spring-security-client/src/main/java/com/springSecurity/client/config/WebSecurityConfig.java
+++ spring-security-client/src/main/java/com/springSecurity/client/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.springSecurity.client.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -38,7 +39,12 @@ class WebSecurityConfig {
                 .disable()
                 .authorizeHttpRequests()
                 .requestMatchers(WHITE_LIST_URLS)
-                .permitAll();
+                .permitAll()
+                .requestMatchers("/api/**").authenticated()
+                .and()
+                .oauth2Login(oauth2login ->
+                        oauth2login.loginPage("/oauth2/authorization/api-client-oidc"))
+                .oauth2Client(Customizer.withDefaults());
         return http.build();
     }
 }
